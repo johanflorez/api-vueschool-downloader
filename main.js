fde6177070b023da18c94ab9456b685b53af57bf
ndriodna <kurodesu6969@gmail.com>
2024-07-14 22:15:39 +0800

    add: puppeteer package and websocket server

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 import http from "http"
 import { routeHandler } from "./src/helper/httpRouter.js"
 import initRouter from "./src/router/router.js"
+import { WebSocketServer } from "ws"
+import setupWS from "./src/ws/wsHandler.js"
 
 initRouter()
 
 const server = http.createServer(routeHandler)
-
+const ws = new WebSocketServer({ server })
+setupWS(ws)
 const port = 3000
 console.log(`server listen: http://localhost:${port}`)
 server.listen(port)
