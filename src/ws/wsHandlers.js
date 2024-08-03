7faf053870b288a5e5395a944e782e8e1dab8549
ndriodna <kurodesu6969@gmail.com>
2024-08-03 15:43:07 +0800

    fix: add middleware config

--- src/ws/wsHandlers.js
+++ src/ws/wsHandlers.js
@@ -1,21 +1,25 @@
-const handlers = {}
+const handlers = []
 
-export function RegisterWsHandler(type, handler) {
-    if (!handlers[type]) {
-        handlers[type] = {}
-    }
-    handlers[type] = handler
+export function RegisterWsHandler(type, ...handler) {
+
+    handlers.push({ type, handler })
 }
 export function wsHandlers(socket, msg, req) {
-    try {
-        const data = JSON.parse(msg)
-        if (handlers[data.type]) {
-            return handlers[data.type](socket, data, req)
+    let index = 0
+    const data = JSON.parse(msg)
+    const handler = handlers.find((r) => r.type === data.type)
+    console.log(handler)
+    if (handler) {
+        const next = () => {
+            if (index < handler.handler.length) {
+                handler.handler[index++](socket, data, req, next)
+            }
         }
+        next()
+    } else {
         socket.send(JSON.stringify({ type: 'error', msg: 'type not found' }))
-    } catch (error) {
-        socket.send(JSON.stringify({ type: 'error', msg: 'data send type not JSON' }))
     }
+    // socket.send(JSON.stringify({ type: 'error', msg: 'data send type not JSON' }))
 
 }
 
