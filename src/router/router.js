9af34e2cc6a5d5bcc9d746005e9a6b3279f5912c
ndriodna <kurodesu6969@gmail.com>
2024-08-03 15:42:25 +0800

    change: middleware name import

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,6 @@
 import { login } from '../controller/UserController.js'
 import { addRoute } from '../helper/httpRouter.js'
-import { AuthMiddleware } from '../helper/authMiddleware.js'
+import { AuthMiddleware } from '../helper/Middleware.js'
 
 export default function initRouter() {
     addRoute('POST', '/login', AuthMiddleware, login)
