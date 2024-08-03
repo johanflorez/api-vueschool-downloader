2ec1bc421490c442c5f69f73d7564afb91529ea1
ndriodna <kurodesu6969@gmail.com>
2024-08-03 15:41:35 +0800

    fix: puppeter launch using args enable features doh cloudflare and cookies

--- src/controller/PuppeteerController.js
+++ src/controller/PuppeteerController.js
@@ -3,7 +3,14 @@ import puppeteer from "puppeteer"
 let Browser = null
 export async function initBrowser() {
     if (!Browser) {
-        Browser = await puppeteer.launch({ headless: true })
+        Browser = await puppeteer.launch({
+            headless: true,
+            args: [
+                '--enable-features=DnsOverHttps,ThirdPartyCookiesEnabled',
+                '--dns-over-https-resolver=https://cloudflare-dns.com/dns-query'
+            ]
+        })
+        await new Promise(resolve => setTimeout(resolve, 5000));
         console.log('init browser')
     }
 
@@ -19,7 +26,7 @@ export async function createPage() {
     if (!Browser) {
         throw new Error('Browser not launched yet')
     }
-    return Browser.newPage()
+    return await Browser.newPage()
 }
 
 export async function log() {
